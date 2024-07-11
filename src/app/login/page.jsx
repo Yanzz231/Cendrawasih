a462b7dda8366a7086cabcb48b7f479b40a904c3
Bronya Vollerei <bronyavollerei@gmail.com>
2024-07-11 23:27:05 +0700

    login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,36 +2,42 @@ import Image from "next/image"
 
 export default function page() {
     return (
-        <section class="bg-gray-50 dark:bg-gray-900">
+        <section class="bg-gray-700">
             <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div class="w-full bg-white rounded-lg border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">  
+                    <div>              
+<div class="flex items-center pt-4 justify-center">
+  <Image class="h-auto max-w-full w-24 flex rounded-lg" src="/image/Dummy.jpg"  width={350} height={350} alt="image description"/>
+</div>
+</div>
+
                     <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                            Sign in to your account
+                        <h1 class="text-xl font-bold text-center leading-tight tracking-tight text-gray-900 md:text-2xl text-black">
+                            Welcome To Cendrawasih
                         </h1>
                         <form class="space-y-4 md:space-y-6" action="#">
                             <div>
-                                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                                <label for="gmail" class="block mb-2 text-sm font-medium text-gray-900 text-black">Your Gmail</label>
+                                <input type="gmail" name="gmail" id="gmail" class="bg-gray-50 border border-gray-400 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="User@gmail.com" required=""/>
                             </div>
                             <div>
-                                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                                <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Your Password</label>
+                                <input type="password" name="password" id="password" placeholder="*******" class="bg-gray-50 border border-gray-400 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required=""/>
                             </div>
                             <div class="flex items-center justify-between">
                                 <div class="flex items-start">
                                     <div class="flex items-center h-5">
-                                        <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
+                                        <input id="rememberme" aria-describedby="rememberme" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300" required=""/>
                                     </div>
                                     <div class="ml-3 text-sm">
-                                        <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                                        <label for="remember" class="text-gray-500">Remember me</label>
                                     </div>
                                 </div>
-                                <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                                <a href="#" class="text-sm font-medium text-primary-600 hover:underline ">Forgot password?</a>
                             </div>
-                            <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                            <button type="submit" class="bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 w-full text-black bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
+                            <p class="text-sm font-light text-black ">
+                                Don’t have an account yet? <a href="#" class="font-medium text-primary-800 hover:underline ">Sign up</a>
                             </p>
                         </form>
                     </div>
