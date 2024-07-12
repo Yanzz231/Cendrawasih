538dff87371c2490e1e5bde2597f2409926a7dd4
Yanzz231 <mrpartys05@gmail.com>
2024-07-12 11:09:36 +0700

    Update Profile

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,48 +1,33 @@
+import Profile from "@/component/profile"
+import SideBar from "@/component/sidebar"
 import Image from "next/image"
 
 export default function page() {
     return (
-        <div className="bg-gray-50">
-            <div class="justify-center flex">
-                <Image class="rounded-full w-24 h-24 mt-8 justify-center flex text-center" src="/image/user.jpg" width={350} height={350} alt="image description" />
-            </div>
-            <div>
-                <p class="text-4xl font-extrabold text-center mt-4">Username</p>
-                <p class="text-2x1 text-gray-500 text-center mt-2">Gmail@gmail.com</p>
-                <p class="text-3x1 text-bold text-center mt-2">4 Follower</p>
-            </div>
-            <div class=" flex justify-center mt-2">
-                <button type="button" class="text-black bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-md rounded-full text-sm me-2 mb-2 px-5 py-2.5 text-center ">Edit Profil</button>
-            </div>
-            <div class="text-center justify-center mt-2">
-                <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-black dark:text-black focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-                    <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                        My post
-                    </span>
-                </button>
-                <button class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-black dark:text-black focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-                    <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                        Liked Post
-                    </span>
-                </button>
-            </div>
-            <div>
-                <div class="justify-center flex gap-2">
-                    <div class="grid grid-cols-2 md:grid-cols-3 gap-2">
-                        <div>
-                            <Image class=" h-auto max-w-full rounded-lg mt-8 justify-center flex text-center" src="/image/Dummy.jpg" width={350} height={350} alt="image description" />
-                        </div>
-                        <div>
-                            <Image class=" h-auto max-w-full rounded-lg mt-8 justify-center flex text-center" src="/image/Dummy.jpg" width={350} height={350} alt="image description" />
-                        </div>
-                        <div>
-                            <Image class=" h-auto max-w-full rounded-lg mt-8 justify-center flex text-center" src="/image/Dummy.jpg" width={350} height={350} alt="image description" />
-                        </div>
-                        <div>
+        <>
+            <SideBar />
+
+            <div className="bg-gray-50">
+                <Profile />
+
+                <div>
+                    <div className="justify-center flex gap-2">
+                        <div className="grid grid-cols-2 md:grid-cols-3 gap-2">
+                            <div>
+                                <Image className=" h-auto max-w-full rounded-lg mt-8 justify-center flex text-center" src="/image/Dummy.jpg" width={350} height={350} alt="image description" />
+                            </div>
+                            <div>
+                                <Image className=" h-auto max-w-full rounded-lg mt-8 justify-center flex text-center" src="/image/Dummy.jpg" width={350} height={350} alt="image description" />
+                            </div>
+                            <div>
+                                <Image className=" h-auto max-w-full rounded-lg mt-8 justify-center flex text-center" src="/image/Dummy.jpg" width={350} height={350} alt="image description" />
+                            </div>
+                            <div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
---
+++ src/component/profile.jsx
@@ -0,0 +1,48 @@
+"use client"
+
+import Image from "next/image"
+import { useState, useEffect } from "react"
+
+
+export default function Profile() {
+
+    const [user, setUser] = useState([])
+
+    const checkUser = async (user, pass) => {
+        const data = { username: user, password: pass }
+        const response = await fetch("/api/signin", {
+            method: "POST",
+            body: JSON.stringify(data)
+        })
+
+        const postData = await response.json()
+        if (postData.status) {
+            setUser(postData.data[0])
+        }
+    }
+
+    useEffect(() => {
+
+        const getAccount = localStorage.getItem('username');
+        const getPassword = localStorage.getItem('password');
+
+        if (getAccount !== null || getPassword !== null) {
+            checkUser(getAccount, getPassword)
+        }
+    }, [])
+
+    return (
+        <>
+            <div className="justify-center flex">
+                <Image className="rounded-full w-44 h-44 mt-8 justify-center flex text-center" src="/image/user.jpg" width={350} height={350} alt="image description" />
+            </div>
+            <div>
+                <p className="text-4xl font-extrabold text-center mt-4 text-black">{user.username}</p>
+                <p className="text-2x1 text-gray-500 text-center mt-2">{user.email}</p>
+            </div>
+            <div className=" flex justify-center mt-2">
+                <button type="button" className="text-black bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-md rounded-full text-sm me-2 mb-2 px-5 py-2.5 text-center ">Edit Profil</button>
+            </div>
+        </>
+    )
+}
