9b7a00b7dc53e908ecaf14a4d781e816575b3d18
Yanzz231 <mrpartys05@gmail.com>
2024-07-12 14:23:22 +0700

    Sign

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -3,9 +3,9 @@ import Image from "next/image"
 
 export default function page() {
     return (
-        <section className="bg-yellow-50">
+        <section className="bg-gray-50">
             <div className="flex flex-col items-center shadow-xl justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <div className="w-full bg-white rounded-lg shadow-xl border md:mt-0 sm:max-w-md xl:p-0 border-gray-700">
+                <div className="w-full bg-white rounded-lg shadow-lg md:mt-0 sm:max-w-md xl:p-0">
                     <div>
                         <div className="flex items-center pt-4 justify-center">
                             <Image className="h-auto max-w-full w-24 flex rounded-lg" src="/image/Logo_Cendrawasih.png" width={350} height={350} alt="image description" />
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -3,9 +3,9 @@ import Image from "next/image"
 
 export default function page() {
     return (
-        <section className="bg-indigo-200">
+        <section className="bg-gray-50 h-screen">
             <div className="flex flex-col items-center justify-center px-4 py-6 mx-auto ">
-                <div className="w-full bg-white rounded-lg border md:mt-0 sm:max-w-md xl:p-0 border-gray-700">
+                <div className="w-full bg-white rounded-lg shadow-lg md:mt-0 sm:max-w-md xl:p-0">
                     <div>
                         <div className="flex items-center pt-4 justify-center">
                             <Image className="h-auto max-w-full w-24 flex rounded-lg" src="/image/Logo_Cendrawasih.png" width={350} height={350} alt="image description" />
