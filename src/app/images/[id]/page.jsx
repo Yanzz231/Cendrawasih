a62e21572a5b621309369d7b6ba474d4d495a4af
Yanzz231 <mrpartys05@gmail.com>
2024-07-12 23:28:41 +0700

    Details

--- src/app/images/[id]/page.jsx
+++ src/app/images/[id]/page.jsx
@@ -1,8 +1,11 @@
 import DetailPost from "@/component/detailPost"
+import SideBar from "@/component/sidebar"
 
 export default function page({ params: { id } }) {
   return (
     <div className="bg-gray-50 h-screen">
+
+      <SideBar />
       <DetailPost id={id} />
     </div>
   )
--- src/component/detailPost.jsx
+++ src/component/detailPost.jsx
@@ -1,8 +1,19 @@
 "use client"
+import { textPopUp } from "@/libs/swal"
 import { Heart } from "@phosphor-icons/react"
 import Image from "next/image"
 import { useRouter } from "next/navigation"
 import { useEffect, useRef, useState } from "react"
+
+export const metadata = {
+    title: "Detail - Cendrawasih",
+    description: "Website Cendrawasih",
+    icons: {
+        icon: "/image/Logo_Cendrawasih.png",
+        shortcut: "/image/Logo_Cendrawasih.png",
+        apple: "/image/Logo_Cendrawasih.png"
+    }
+};
 
 export default function DetailPost({ id }) {
     const [data, setData] = useState([])
@@ -21,6 +32,7 @@ export default function DetailPost({ id }) {
 
     const handleSubmit = async (event) => {
         event.preventDefault()
+        if (input.length < 3) return textPopUp("Error", "Too short comment", "error")
         const inputComment = { id_post: data.id, value: input, username: user1.username }
         setComment(prevData => [...(prevData || []), inputComment]);
 
@@ -138,55 +150,48 @@ export default function DetailPost({ id }) {
     }, [])
 
     return (
-        <div className="bg-gray-50">
-            <div className="bg-gray-50 ">
-                <div className="flex flex-col items-center">
-                    <div className="flex justify-center mr-200 mb-2">
-                        <p className="text-gray-900 text-3xl">{data.title}</p>
-                    </div>
-                    <div className="flex justify-center mr-200 mb-2">
-                        <p className="text-gray-900 text-3xl">{data.description}</p>
-                    </div>
-                    <div className="flex justify-center items-center">
-                        <Image className="rounded-lg mt-4" src={data.image} alt="image description" width={350} height={350} />
-                    </div>
+        <div className="bg-gray-50 flex md:flex-row flex-col items-center sm:ml-72 pt-20">
+            <div className="md:w-1/2 md:pr-4">
+                <div className="flex flex-col justify-center ">
+                    <Image className="rounded-lg flex px-3 pl-6  items-center justify-center mt-4 " src={data.image} alt="description" width={1500} height={1500} />
+                    <h1 className="pl-6 px-3 font-bold text-3xl text-black mt-2">{data.title}</h1>
+                    <p className="text-black pl-6 px-3 text-sm mt-4">{data.description}</p>
                 </div>
             </div>
-            <div className="bg-gray-50">
-                <div className={`flex justify-center items-center mt-8 ${like ? "text-pink-500" : "text-black"}`}>
-                    <button onClick={handleLike} className="flex gap-2 items-center">
-                        {like1} <Heart size={32} />
-                    </button>
-                </div>
-                <div className="flex justify-center items-center mt-8 mb-10">
-                    {user.image === "defaultPicture" ? (
-                        <Image className="rounded-full w-10 h-10" src="/image/user.jpg" width={40} height={40} alt="image description" />
-                    ) : (
-                        <Image className="rounded-full w-10 h-10" src={user.image} width={40} height={40} alt="image description" />
-                    )}
-                    <div className="ml-4">
-                        <p className="text-gray-900 text-xl">Uploaded by {user.username}</p>
-                    </div>
-                </div>
-            </div>
-
-            <div className="flex flex-col items-center overflow-auto gap-4" style={{ maxHeight: '300px', width: '100%' }}>
-                {comment.map((res, index) => {
-                    return (
-                        <div key={index} className="flex items-start gap-2.5 w-full justify-center">
-                            <div className="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
-                                <div className="flex items-center space-x-2 rtl:space-x-reverse">
-                                    <span className="text-sm font-semibold text-gray-900 dark:text-white">{res.username}</span>
-                                </div>
-                                <p className="text-sm font-normal py-2.5 text-gray-900 dark:text-white">{res.value}</p>
-
-                            </div>
+            <div className="md:w-1/2 md:pl-4">
+                <div className="bg-gray-50">
+                    <div className="flex justify-center items-center mt-8 mb-10">
+                        {user.image === "defaultPicture" ? (
+                            <Image className="rounded-full w-10 h-10" src="/image/user.jpg" width={40} height={40} alt="image description" />
+                        ) : (
+                            <Image className="rounded-full w-10 h-10" src={user.image} width={40} height={40} alt="image description" />
+                        )}
+                        <div className="ml-1 mb-1">
+                            <p className="text-gray-700 text-xl">Uploaded by {user.username}</p>
                         </div>
-                    )
-                })}
-            </div>
+                    </div>
+                    <div className={`flex justify-center items-center mt-8 mb-8 ${like ? "text-pink-500" : "text-black"}`}>
+                        <button onClick={handleLike} className="flex gap-2 items-center">
+                            {like1} <Heart size={32} />
+                        </button>
+                    </div>
+                </div>
+                <div className="flex flex-col items-center overflow-auto gap-4" style={{ maxHeight: '300px', width: '100%' }}>
+                    {comment.map((res, index) => {
+                        return (
+                            <div key={index} className="flex items-start gap-2.5 w-full justify-center">
+                                <div className="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
+                                    <div className="flex items-center space-x-2 rtl:space-x-reverse">
+                                        <span className="text-sm font-semibold text-gray-900 dark:text-white">{res.username}</span>
+                                    </div>
+                                    <p className="text-sm font-normal py-2.5 text-gray-900 dark:text-white">{res.value}</p>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
 
-            {/* <div className="flex justify-center mt-3">
+                {/* <div className="flex justify-center mt-3">
                 <div className="flex items-end gap-2.5">
                     <div className="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
                         <div className="flex items-center space-x-2 rtl:space-x-reverse">
@@ -201,19 +206,24 @@ export default function DetailPost({ id }) {
                 </div>
             </div> */}
 
-            <div className="flex justify-center">
-                <form className="py-2 w-auto px-4 mt-4 bg-gray-50">
-                    <div className="flex mx-4 items-center">
-                        <textarea ref={inputRef} value={input} onChange={handleInput} id="comment" rows="1" className="block w-96 mt-3 p-2.5 text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Your Message"></textarea>
-                        <button onClick={handleSubmit} type="submit" className="ml-auto p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100">
-                            <svg className="w-5 h-5 rotate-90 rtl:-rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-                                <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z" />
-                            </svg>
-                            <span className="sr-only">Send message</span>
-                        </button>
-                    </div>
-                </form>
+                <div className="flex justify-center">
+                    <form className="py-2 w-auto px-4 mt-4 bg-gray-50">
+                        <div className="flex mx-4 items-center">
+                            <textarea ref={inputRef} value={input} onChange={handleInput} id="comment" rows="1" className="block w-96 mt-3 p-2.5 text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Your Message"></textarea>
+                            <button onClick={handleSubmit} type="submit" className="ml-auto p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100">
+                                <svg className="w-5 h-5 rotate-90 rtl:-rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                                    <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z" />
+                                </svg>
+                                <span className="sr-only">Send message</span>
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     )
-} 
+}
+
+
+
+
